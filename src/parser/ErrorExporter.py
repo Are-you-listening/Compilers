5034c7f63f78c6835b354eddf75a055b3ea42a09
watson <kars@duck.com>
2024-02-28 17:10:08 +0100

    added error to error =)

--- src/parser/ErrorExporter.py
+++ src/parser/ErrorExporter.py
@@ -2,31 +2,31 @@
 class ErrorExporter:
     @staticmethod
     def mainNotFound():
-        print("main function not found")
+        print("[ Error ] main function not found")
 
     @staticmethod
     def invalidOperatorFloat(operator: str):
-        print(f"invalid float operation {operator}")
+        print(f"[ Error ] invalid float operation {operator}")
 
     @staticmethod
     def invalidOperatorPtr(operator: str):
-        print(f"invalid ptr operation {operator}")
+        print(f"[ Error ] invalid ptr operation {operator}")
 
     @staticmethod
     def invalidRvalue(identifier: str):
-        print(f"invalid assignment to {identifier}")
+        print(f"[ Error ] invalid assignment to {identifier}")
 
     @staticmethod
     def uninitializedVariable(identifier: str):
-        print(f"use of uninitialized variable {identifier}")
+        print(f"[ Error ] use of uninitialized variable {identifier}")
 
     @staticmethod
     def undeclaredVariable(identifier: str):
-        print(f"use of undeclared variable {identifier}")
+        print(f"[ Error ] use of undeclared variable {identifier}")
 
     @staticmethod
     def constComplaint(identifier: str, type: str):
-        print(f"Assignment to variable {identifier} which is of type {type}")
+        print(f"[ Error ] Assignment to variable {identifier} which is of type {type}")
 
     @staticmethod
     def redefinition(linenr: str, pos: str, variable: str, type: str):
