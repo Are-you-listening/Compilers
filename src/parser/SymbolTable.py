1e4749d914df99640c1f05d2bdd9694560ca1e39
watson <kars@duck.com>
2024-02-28 16:57:49 +0100

    Added functionality:  1. to traverse tables up & downwards  2. to check if an entry exists in the current or global scope

--- src/parser/SymbolTable.py
+++ src/parser/SymbolTable.py
@@ -1,17 +1,19 @@
 from src.parser.ErrorExporter import *
+from src.parser.ErrorExporter import *
 
 class SymbolEntry:
-    def __init__(self, fitype: str, datatype: str, name: str, const: bool, value, firstUsed , firstDeferred):
+    def __init__(self, fitype: str, datatype: str, name: str, const: bool, value, firstUsed, firstDeferred):
         self.fitype = fitype
         self.type = datatype
         self.name = name
         self.const = const
         self.value = value
-        self.firstUsed = firstUsed # The node this Entry is first used
-        self.firstDeferred = firstDeferred # The node this Enry is first deferred
+        self.firstUsed = firstUsed  # The node this Entry is first used
+        self.firstDeferred = firstDeferred  # The node this Enry is first deferred
 
     def print(self):
-        print(self.fitype, self.type, self.name, "| const = ", self.const, " | value = ", self.value, self.firstUsed, self.firstDeferred)
+        print(self.fitype, self.type, self.name, "| const = ", self.const, " | value = ", self.value, self.firstUsed,
+              self.firstDeferred)
 
 
 class SymbolTable:
@@ -21,6 +23,12 @@ class SymbolTable:
         self.next = []
 
     def add(self, entry: SymbolEntry):
+        existing = self.symbols.get(entry.name)
+        entry.print()
+        if existing is not None: # Redefinition of variable
+            ErrorExporter.redefinition(None,None,existing.type,existing.name)
+            return
+
         self.symbols[entry.name] = entry
 
     def remove(self, symbol: SymbolEntry):
@@ -29,20 +37,53 @@ class SymbolTable:
     def getEntry(self, name):
         return self.symbols.get(name)
 
-    def print(self):
-        print(self.symbols)
-
-    def traverse(self,function, printEntry: bool):
+    def print(self,printEntry=False):
         if printEntry:
             for entry in self.symbols.values():
                 entry.print()
+        print(self.symbols)
 
-        for child in self.next:
-            if printEntry:
-                for entry in child.symbols.values():
-                    entry.print()
-            child.traverse(function,printEntry)
+    def traverse(self, function, printEntry: bool, up: bool):
+        """
 
-    def nextTable(self,next):
+        :param function: function to execute
+        :param printEntry: toggle the print of entries
+        :param up: go up or down the list
+        :return:
+        """
+        if up:
+            self.print(printEntry)
+            self.prev.traverse(function,printEntry,up)
+        else:
+            self.print(printEntry)
+
+            for child in self.next:
+                self.print(printEntry)
+                child.traverse(function, printEntry)
+
+    def nextTable(self, next):
         """Add a new table as child"""
         self.next.append(next)
+
+    def exists(self,entryname):
+        """
+        Check if a given symbolEntry already exists in the scope
+        :param entryname:
+        :return:
+        """
+        if self.entryExists(entryname):
+            return True
+        if self.prev!=None:
+            self.prev.traverse(self.entryExists(entryname), False, True) # Else: traverse upwards
+        else:
+            return False
+
+    def entryExists(self,entryname):
+        """
+        Check if a given symbolEntry already exists in the current table
+        :param entryname:
+        :return:
+        """
+        if self.symbols.get(entryname) is None:
+            return False
+        return True
