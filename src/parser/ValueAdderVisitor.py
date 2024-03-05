0df7185b3f0c130327a28c8af12713b860e41438
tibov <tibo.verreycken@telenet.be>
2024-03-05 19:20:45 +0100

    mini changes and fixes

--- src/parser/ValueAdderVisitor.py
+++ src/parser/ValueAdderVisitor.py
@@ -43,6 +43,6 @@ class ValueAdderVisitor(ASTVisitor):
     def visitNodeTerminal(self, node: ASTNodeTerminal):
         if node.type == "IDENTIFIER":
             # if it is a variable, and it is not the node where it is first declared -> update firstUsed if necessary
-            entry = node.getSymbolTable().symbols[node.text]
+            entry = node.getSymbolTable().getEntry(node.text)
             if node != entry.firstDeclared:
                 entry.firstUsed = node
