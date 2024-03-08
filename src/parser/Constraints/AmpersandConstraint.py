3292b77e7d35b4a4e2bdab83ba35dcb08c83d795
watson <kars@duck.com>
2024-03-08 17:16:57 +0100

    small cleanup

--- src/parser/Constraints/AmpersandConstraint.py
+++ src/parser/Constraints/AmpersandConstraint.py
@@ -1,20 +1,19 @@
-
-
 from src.parser.Constraints.Constraint import *
 from src.parser.ErrorExporter import *
-from src.parser.helper import *
 
 
 class AmpersandConstraint(Constraint):
     """
     Verifies the integrity of the const variables
     """
+
     def __init__(self):
         super().__init__()
 
     def checkTerminalNode(self, node: ASTNodeTerminal):
         if node.text == "&":
             rsibl = node.getSiblingNeighbour(1)
-            if rsibl.text == "Expr" or node.symbol_table.getEntry(rsibl.text) == None: # This operation is only applicapble on a single identifier, not on a literal
+            if rsibl.text == "Expr" or node.symbol_table.getEntry(
+                    rsibl.text) == None:  # This operation is only applicapble on a single identifier, not on a literal
                 ErrorExporter.invalidOperatorPtr("", node.linenr)
                 self.rejected = True
