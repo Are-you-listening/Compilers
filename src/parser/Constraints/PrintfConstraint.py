3a39cca2efdfb0d5051da3f7ce3e84687eadf769
watson <kars@duck.com>
2024-03-10 19:34:24 +0100

    compliance

--- src/parser/Constraints/PrintfConstraint.py
+++ src/parser/Constraints/PrintfConstraint.py
@@ -4,11 +4,12 @@ from src.parser.ErrorExporter import *
 
 class PrintfConstraint(Constraint):
     """
-    Verifies if the typecorrectnes use by each printf function call
+    Verifies the correct type was used for each printf function call
     """
 
     def __init__(self):
         super().__init__()
+        self.rejected = False
 
     def checkNode(self, node: ASTNodeTerminal):
         if node.text == "printf":
@@ -17,6 +18,7 @@ class PrintfConstraint(Constraint):
 
             if printtype != variabletype:
                 ErrorExporter.invalidOperation(node.children[0].linenr, "printf", printtype, variabletype)
+                sys.exit()
 
     def getPrintType(self, text):
         if text == "%s":
