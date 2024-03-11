ab972e23ce8b5e66658194df5fd902eea8b5cdd5
watson <kars@duck.com>
2024-03-11 23:13:37 +0100

    cleanup

--- src/parser/ASTConversion.py
+++ src/parser/ASTConversion.py
@@ -4,151 +4,198 @@ from src.parser.CTypes.COperationHandler import *
 from src.parser.Tables.SymbolTypePtr import *
 
 
-class ASTConversion(ASTVisitor):
+class ASTConversion2(ASTVisitor):
     """
     Makes implicit conversions explicit
     """
     def __init__(self):
         self.rc = RichnessChecker(types)
 
+        """
+        map node on its resulting type
+        """
+        self.type_mapping = {}
+        self.incompatible = {
+            "FLOAT": ["%", "|", "&", "~", "CHAR"],  # FLOAT & CHAR are always incompatible
+            "PTR": ["/", "^", ">>", "<<", "%", "|", "~"]
+        }
+
     def visit(self, ast: AST):
         root = ast.root
-        self.preorder(root)
+        self.postorder(root)
 
     def visitNode(self, node: ASTNode):
-        if node.text == "Declaration" and node.getChildAmount() == 2:
+        if node.text == "Dereference":
+            child = node.getChild(0)
+            data_type, ptrs = self.type_mapping[child]
             """
-            situation when declaration
+            remove 1 ptr
             """
-            child = node.getChild(1)
-            poorest = self.getPoorestType(child)
-            child_type = poorest
-
-            to_type_tree = node.getChild(0)
-            if to_type_tree.getChild(0).text.upper() != child_type:
-                new_node = ASTNode("Conversion", node, child.getSymbolTable())
-                type_node = ASTNode("Type", new_node, new_node.getSymbolTable())
-                new_node.addChildren(type_node)
-
-                for t_child in to_type_tree.children:
-                    type_node.addChildren(ASTNodeTerminal(t_child.text, type_node, type_node.getSymbolTable(), t_child.type,
-                                                          t_child.operation_type))
-                child.addNodeParent(new_node)
-
-        if node.text == "Assignment" and node.getChildAmount() == 2:
-            """
-            situation Assignment
-            """
-            type_child = node.getChild(0)
-            child = node.getChild(1)
-            data_type = type_child.getSymbolTable().getEntry(type_child.text).getType()
-            if self.getPoorestType(child) != data_type:
-                new_node = ASTNode("Conversion", node, child.getSymbolTable())
-                type_node = ASTNode("Type", new_node, new_node.getSymbolTable())
-                new_node.addChildren(type_node)
-
-                type_node.addChildren(
-                    ASTNodeTerminal(data_type, type_node, type_node.getSymbolTable(), data_type,
-                                    None))
-
-                child.addNodeParent(new_node)
-
-    def visitNodeTerminal(self, node: ASTNodeTerminal):
-        parent = node.parent
-
-        if parent.text == "Expr":
-            poorest = self.getPoorestType(parent)
-            node_type = node.type
-            if node_type == "IDENTIFIER":
-                type_entry = node.getSymbolTable().getEntry(node.text)
-                node_type = type_entry.getType().upper()
-
-            """
-            We check for nodes that have a type, if so we check if this type is the poorest type
-            If not it will implicitly be converted to the poorest type
-            """
-
-            if poorest != node_type and node_type in types:
-
-                new_node = ASTNode("Conversion", parent, node.getSymbolTable())
-                type_node = ASTNode("Type", new_node, new_node.getSymbolTable())
-                new_node.addChildren(type_node)
-                type_node.addChildren(ASTNodeTerminal(node_type, type_node, type_node.getSymbolTable(), node_type))
-                node.addNodeParent(new_node)
-
-    def getPoorestType(self, node: ASTNode):
-        """
-        Function to get the poorest type of the subtree
-        :param node: root of the subtree
-        :return: poorest type
-        """
-        if isinstance(node, ASTNodeTerminal):
-            t_type = node.type
-
-            if t_type == "IDENTIFIER":
-                """
-                return type corresponding to the value of the Identifier
-                """
-                type_entry = node.getSymbolTable().getEntry(node.text)
-                return type_entry.getType()
-
-            if t_type not in types:
-                return None
-            return node.type
+            ptrs = ptrs[:-1]
+            self.type_mapping[node] = (data_type, ptrs)
+            return
 
         if node.text == "Conversion":
             type_value = node.getChild(0)
-            return self.getPoorestType(type_value)
+            data_type, ptrs = self.calculateType(type_value)
+            self.type_mapping[node] = (data_type, ptrs)
+            return
 
-        if node.text == "Dereference":
-            deref = self.getDereference(node)
-            if not isinstance(deref, str):
-                return "PTR"
-            return deref
+        if node.text not in ("Literal", "Expr", "Declaration", "Assignment"):  # Exclude "Function" for now
+            return
 
-        poorest_child = None
-        for child_index in range(node.getChildAmount()):
-            child = node.getChild(child_index)
-            child_type = self.getPoorestType(child)
-            if child_type is None:
+        """
+        take the poorest type, if types are inconsistent add implicit conversion
+        """
+        current_poorest = None
+        current_poorest_ptrs = ""
+
+        checkCompatibility = []
+
+        for child in node.children:
+            type_tup = self.type_mapping.get(child, None)
+            if type_tup is None:
+                checkCompatibility.append(child.text)
                 continue
 
-            if poorest_child is None:
-                poorest_child = child_type
+            data_type, ptrs = type_tup
+
+            """
+            ignore ptrs for now
+            """
+
+            if current_poorest is None:
+                current_poorest = data_type
+                current_poorest_ptrs = ptrs
             else:
-                poorest_child = self.rc.get_poorest(poorest_child, child_type)
 
-        return poorest_child
+                if current_poorest_ptrs == ptrs:  # If the amount of "*" is the same; nothing to worry about
+                    current_poorest = self.rc.get_poorest(data_type, current_poorest)
+                else:
+                    # Handle errors
+                    if (len(current_poorest_ptrs)>=2 and data_type == "FLOAT" and len(ptrs)==0):
+                        print(ptrs)
+                        ErrorExporter.invalidOperation(node.linenr, "", "PTR", "FLOAT")
+                    # else:  # PTR and INT/CHAR can convert in between each other, however a warning will be shown
+                    #     type1 = current_poorest + current_poorest_ptrs
+                    #     type2 = data_type + self.type_mapping[child][1]
+                    #     if type1[len(type1) - 1] == '*':
+                    #         type1 = type1[:-1]
+                    #     if type2[len(type2) - 1] == '*':
+                    #         type2 = type2[:-1]
+                    #
+                    #     if type1 != type2:
+                    #         ErrorExporter.conversionWarning(node.linenr, type1, type2)
 
-    def getDereference(self, node: ASTNode):
+        checkCompatibility.append(current_poorest)  # Check for illegal operations
+
         """
-        get to which the subtree references
-        for example 'int a' references to int
-        int* a =0;, *(a+1) references to int too
+        Verify if the operators are allowed | Semantic Analyse
+        """
+        if not self.compatible(checkCompatibility):
+            if len(checkCompatibility) == 3:  # Binary operation
+                ErrorExporter.invalidOperation(node.linenr, checkCompatibility[1], checkCompatibility[0],
+                                               checkCompatibility[2])
+            elif checkCompatibility[0] not in types:  # Operator is on index 0
+                ErrorExporter.invalidOperation(node.linenr, checkCompatibility[0], checkCompatibility[1],
+                                               "")
+            elif checkCompatibility[1] not in types:  # Operator is on index 1
+                ErrorExporter.invalidOperation(node.linenr, checkCompatibility[1], checkCompatibility[1],
+                                               "")
+
+        """
+        for declaration and assignment the type is the type of the value that is declared/assigned (and not the necesarily the poorest type)
+        """
+        if node.text in ("Declaration", "Assignment", "Function"):
+            assign_node = node.getChild(0)
+
+            while assign_node.text == "Dereference":
+                assign_node = assign_node.getChild(0)
+
+            entry = assign_node.getSymbolTable().getEntry(assign_node.text)
+
+            data_type, ptrs = entry.getPtrTuple()
+            current_poorest = data_type
+            current_poorest_ptrs = ptrs
+
+        """
+        add implict conversions as explicit
+        """
+        for child in node.children:
+            type_tup = self.type_mapping.get(child, None)
+            if type_tup is None:
+                continue
+
+            data_type, ptrs = type_tup
+            if data_type != current_poorest:
+
+                new_node = ASTNode("Conversion", node, child.getSymbolTable())
+                type_node = ASTNode("Type", new_node, new_node.getSymbolTable())
+                new_node.addChildren(type_node)
+
+                """
+                add datatype
+                """
+                type_node.addChildren(
+                    ASTNodeTerminal(current_poorest, type_node, type_node.getSymbolTable(), "Not Used",
+                                    None))
+
+                for t_child in current_poorest_ptrs.split():
+                    type_node.addChildren(
+                        ASTNodeTerminal(t_child, type_node, type_node.getSymbolTable(), "Not Used",
+                                        None))
+                child.addNodeParent(new_node)
+
+        self.type_mapping[node] = (current_poorest, current_poorest_ptrs)
+
+    def visitNodeTerminal(self, node: ASTNodeTerminal):
+
+        if node.type == "IDENTIFIER":
+            type_entry = node.getSymbolTable().getEntry(node.text)
+            data_type, ptrs = type_entry.getPtrTuple()
+
+            """
+            Use LLVM ptr format
+            """
+            ptrs += "*"
+
+            self.type_mapping[node] = (data_type, ptrs)
+
+        elif node.type in types:
+            self.type_mapping[node] = (node.type, '*')
+
+    def calculateType(self, node: ASTNode):
+        """
+        provided node is a type node
         :param node:
         :return:
         """
 
-        if node.text == "Dereference":
-            child = node.getChild(0)
+        if node.text != "Type":
+            raise Exception("wrong node type")
 
-            type_obj = self.getDereference(child)
-            if isinstance(type_obj, SymbolTypePtr):
-                return type_obj.deReference()
-            elif isinstance(type_obj, SymbolType):
-                return type_obj.getType()
-
-            return None
-
-        if isinstance(node, ASTNodeTerminal) and node.type == "IDENTIFIER":
-            return node.getSymbolTable().getEntry(node.text).getTypeObject()
-
-        deref_type = None
+        data_type = ""
+        ptrs = ""
         for child in node.children:
-            type_obj = self.getDereference(child)
+            if child.text.upper() in types:
+                data_type = child.text.upper()
 
-            if deref_type is not None and not deref_type.getType() == type_obj.getType():
-                ErrorExporter.wrongRefCombination(deref_type.getType(), type_obj.getType())
+            if child.text == "*":
+                ptrs += "*"
 
-            deref_type = type_obj
+        return data_type, ptrs
 
+    def compatible(self, items):
+        """
+        Check the blacklist for absolute incompatible operations or types
+        :param items:
+        :return:
+        """
+        for item in items:  # Make all combinations of items
+            for item2 in items:
+                if item != item2:  # Except the combination when it has itsself twice
+                    if item in self.incompatible.keys() and item2 in self.incompatible[item]:  # An incompatible match
+                        return False
+                    elif item2 in self.incompatible.keys() and item in self.incompatible[item2]:
+                        return False
+        return True
--- src/parser/ErrorExporter.py
+++ src/parser/ErrorExporter.py
@@ -16,43 +16,64 @@ class ErrorExporter:
     @staticmethod
     def mainNotFound():
         print("[ Error ] main function not found", file=sys.stderr)
+        sys.exit()
 
     @staticmethod
     def invalidOperatorFloat(operator: str):
         print(f"[ Error ] invalid float operation {operator}",file=sys.stderr)
+        sys.exit()
+
 
     @staticmethod
     def invalidOperation(linenr: str, operator: str, type: str, type2: str):
         print(f"[ Error ] line {linenr}: invalid operation {operator} on type(s): {type}  {type2}",file=sys.stderr)
+        sys.exit()
+
 
     @staticmethod
     def invalidOperatorPtr(operator: str, linenr):
         print(f"[ Error ] line {linenr}: invalid ptr operation {operator}",file=sys.stderr)
+        sys.exit()
+
 
     @staticmethod
     def invalidRvalue(identifier: str):
         print(f"[ Error ] invalid assignment to {identifier}",file=sys.stderr)
+        sys.exit()
+
 
     @staticmethod
     def uninitializedVariable(identifier: str):
         print(f"[ Error ] use of uninitialized variable {identifier}",file=sys.stderr)
+        sys.exit()
+
 
     @staticmethod
     def undeclaredVariable(identifier: str):
         print(f"[ Error ] use of undeclared variable {identifier}",file=sys.stderr)
+        sys.exit()
+
 
     @staticmethod
     def constComplaint(linenr: str, identifier: str, type: str):
         print(f"[ Error ] line {linenr}: Manipulation of variable {identifier} which is of type {type}",file=sys.stderr)
+        sys.exit()
+
 
     @staticmethod
     def redefinition(linenr: str, type: str, variable: str):
         print(f"[ Error ] line {linenr}: redefinition or redeclaration of {type} {variable}",file=sys.stderr)
+        sys.exit()
+
 
     @staticmethod
     def wrongRefCombination(type1: str, type2: str):
         print(f"[ Error ] reference types do not match: {type1} and {type2}", file=sys.stderr)
+        sys.exit()
+
 
     @staticmethod
     def conversionWarning(linenr: str, type1: str, type2: str):
         print(f"[ Warning ] line {linenr}: Types do not match properly, a conversion was made on types: {type1} and {type2}")
+        sys.exit()
+
