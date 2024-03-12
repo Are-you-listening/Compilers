ee74236f05ff7ceefa93f1da2e64e0c880c24468
watson <kars@duck.com>
2024-03-12 15:47:03 +0100

    set prints back to regular

--- src/parser/ErrorExporter.py
+++ src/parser/ErrorExporter.py
@@ -2,11 +2,11 @@ from antlr4.tree.Tree import ParseTreeListener
 from antlr4.error.ErrorListener import ErrorListener
 import sys
 
-
 class EListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         #        print("[ Syntax Error ] line " + str(line) + ":" + str(column) + " " + msg, file=sys.stderr)
-        print(f"[ Syntax Error ] line {line}:{column} invalid symbol: '{offendingSymbol.text}'", file=sys.stderr)
+        print(f"[ Syntax Error ] line {line}:{column} invalid symbol: '{offendingSymbol.text}'")
+        #print(f"[ Syntax Error ] line {line}:{column} invalid symbol: '{offendingSymbol.text}'", file=sys.stderr)
 
 
 class ErrorExporter:
@@ -15,61 +15,71 @@ class ErrorExporter:
 
     @staticmethod
     def mainNotFound():
-        print("[ Error ] main function not found", file=sys.stderr)
-        sys.exit()
+        print("[ Error ] main function not found")
+        #print("[ Error ] main function not found", file=sys.stderr)
+        #sys.exit()
 
     @staticmethod
     def invalidOperatorFloat(operator: str):
-        print(f"[ Error ] invalid float operation {operator}",file=sys.stderr)
-        sys.exit()
+        print(f"[ Error ] invalid float operation {operator}")
+        #print(f"[ Error ] invalid float operation {operator}",file=sys.stderr)
+        #sys.exit()
 
 
     @staticmethod
     def invalidOperation(linenr: str, operator: str, type: str, type2: str):
-        print(f"[ Error ] line {linenr}: invalid operation {operator} on type(s): {type}  {type2}",file=sys.stderr)
-        sys.exit()
+        print(f"[ Error ] line {linenr}: invalid operation {operator} on type(s): {type}  {type2}")
+        #print(f"[ Error ] line {linenr}: invalid operation {operator} on type(s): {type}  {type2}",file=sys.stderr)
+        #sys.exit()
 
 
     @staticmethod
     def invalidOperatorPtr(operator: str, linenr):
-        print(f"[ Error ] line {linenr}: invalid ptr operation {operator}",file=sys.stderr)
-        sys.exit()
+        print(f"[ Error ] line {linenr}: invalid ptr operation {operator}")
+        #print(f"[ Error ] line {linenr}: invalid ptr operation {operator}",file=sys.stderr)
+        #sys.exit()
 
 
     @staticmethod
     def invalidRvalue(identifier: str):
-        print(f"[ Error ] invalid assignment to {identifier}",file=sys.stderr)
-        sys.exit()
+        print(f"[ Error ] invalid assignment to {identifier}")
+        #print(f"[ Error ] invalid assignment to {identifier}",file=sys.stderr)
+        #sys.exit()
 
 
     @staticmethod
     def uninitializedVariable(identifier: str):
-        print(f"[ Error ] use of uninitialized variable {identifier}",file=sys.stderr)
-        sys.exit()
+        print(f"[ Error ] use of uninitialized variable {identifier}")
+        #print(f"[ Error ] use of uninitialized variable {identifier}",file=sys.stderr)
+        #sys.exit()
 
 
     @staticmethod
     def undeclaredVariable(identifier: str):
-        print(f"[ Error ] use of undeclared variable {identifier}",file=sys.stderr)
-        sys.exit()
+        print(f"[ Error ] use of undeclared variable {identifier}")
+        #print(f"[ Error ] use of undeclared variable {identifier}",file=sys.stderr)
+        #sys.exit()
 
 
     @staticmethod
     def constComplaint(linenr: str, identifier: str, type: str):
-        print(f"[ Error ] line {linenr}: Manipulation of variable {identifier} which is of type {type}",file=sys.stderr)
-        sys.exit()
+        print(f"[ Error ] line {linenr}: Manipulation of variable {identifier} which is of type {type}")
+        #print(f"[ Error ] line {linenr}: Manipulation of variable {identifier} which is of type {type}",file=sys.stderr)
+        #sys.exit()
 
 
     @staticmethod
     def redefinition(linenr: str, type: str, variable: str):
-        print(f"[ Error ] line {linenr}: redefinition or redeclaration of {type} {variable}",file=sys.stderr)
-        sys.exit()
+        print(f"[ Error ] line {linenr}: redefinition or redeclaration of {type} {variable}")
+        #print(f"[ Error ] line {linenr}: redefinition or redeclaration of {type} {variable}",file=sys.stderr)
+        #sys.exit()
 
 
     @staticmethod
     def wrongRefCombination(type1: str, type2: str):
-        print(f"[ Error ] reference types do not match: {type1} and {type2}", file=sys.stderr)
-        sys.exit()
+        print(f"[ Error ] reference types do not match: {type1} and {type2}")
+        #print(f"[ Error ] reference types do not match: {type1} and {type2}", file=sys.stderr)
+        #sys.exit()
 
 
     @staticmethod
