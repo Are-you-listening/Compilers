9803b137002c402820c9a72c6f315807a2b4c9c2
watson <kars@duck.com>
2024-03-15 14:43:25 +0100

    we can now run all testfiles using the testcase

--- src/TestFiles/CatchAllTest.py
+++ src/TestFiles/CatchAllTest.py
@@ -3,6 +3,7 @@ from src.main.__main__ import main
 import os
 import sys
 import logging
+import subprocess
 
 
 class CatchAllTest(unittest.TestCase):
@@ -16,11 +17,15 @@ class CatchAllTest(unittest.TestCase):
         for root, dirs, filenames in os.walk(path):
             for file in filenames:
                 if file.endswith("c"):
-                    print(root + "/" + file)
+                    #print(root + "/" + file)
                     self.runAST(root + "/" + file)
 
 
+
     def runAST(self, file_name):
+        subprocess.run(f"""clang-14 -S -emit-llvm {file_name} -o {file_name[:-2]}.ll""",
+                             shell=True, capture_output=True)
         main([0, "--input", file_name, "--render_ast", file_name[:-2]+"ASTVisual", "--render_symb", file_name[:-2]+"SymbolTable",
-              "--target_llvm", file_name[:-2] + "LLVM"], True)
+              "--target_llvm", file_name[:-2] + "LLVM.ll"], True)
+
 
