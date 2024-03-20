7b902134e2df9048f897e6baa417d16a95ebfe0f
watson <kars@duck.com>
2024-03-20 22:34:30 +0100

    completed testing

--- src/TestFiles/LLVMTests/CompareAll.py
+++ src/TestFiles/LLVMTests/CompareAll.py
@@ -29,7 +29,7 @@ class CompareAll(unittest.TestCase):
                     filename = root + "/" + file
                     print(filename)
 
-                    if filename in ["testfiles/basic_tests_123/proj2_man_pass_conversionExplicitTypeCast.c","testfiles/basic_tests_123/proj2_man_pass_advancedPointerOperations.c","testfiles/basic_tests_123/proj2_opt_pass_constPointerToNonConstPointer1.c","testfiles/basic_tests_123/proj2_opt_pass_constPointerToNonConstPointer2.c","testfiles/basic_tests_123/proj2_man_pass_conversionImplicit.c","testfiles/advanced_tests/SemanticErrors/incompatibleTypes1.c"]:
+                    if filename in ["testfiles/basic_tests_123/proj2_man_pass_conversionExplicitTypeCast.c","testfiles/basic_tests_123/proj2_man_pass_advancedPointerOperations.c","testfiles/basic_tests_123/proj2_opt_pass_constPointerToNonConstPointer1.c","testfiles/basic_tests_123/proj2_opt_pass_constPointerToNonConstPointer2.c","testfiles/basic_tests_123/proj2_man_pass_conversionImplicit.c"]:
                         continue
                     if filename.startswith("testfiles/advanced_tests/"):
                         continue
