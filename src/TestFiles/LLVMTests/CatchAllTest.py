d0571c3006364363a604a8e8d0535429b62487ca
watson <kars@duck.com>
2024-03-20 14:51:14 +0100

    added llvm test

--- src/TestFiles/LLVMTests/CatchAllTest.py
+++ src/TestFiles/LLVMTests/CatchAllTest.py
@@ -1,3 +1,4 @@
+import filecmp
 import unittest
 from src.main.__main__ import main
 import os
@@ -10,7 +11,7 @@ from src.llvm_target.LLVMSingleton import *
 class CatchAllTest(unittest.TestCase):
     def testEvaluateResults(self):
         """
-        This testcase will run all tests
+        This testcase will run all files in the testfiles/ folder
         :return:
         """
 
@@ -18,8 +19,9 @@ class CatchAllTest(unittest.TestCase):
         for root, dirs, filenames in os.walk(path):
             for file in filenames:
                 if file.endswith(".c"):
-                    print(root + "/" + file)
-                    self.runAST(root + "/" + file)
+                    filename = root + "/" + file
+                    print(filename)
+                    self.runAST(filename)
 
 
 
@@ -28,15 +30,20 @@ class CatchAllTest(unittest.TestCase):
         LLVMSingleton.getInstance().clear()
 
         # Specify filenames we don't want to run for now
-        if file_name in []:
+        if file_name in ["testfiles/basic_tests_123/proj2_man_pass_conversionExplicitTypeCast.c"]:
             return
 
         if file_name.startswith("testfiles/ownTests/proj1"):
             return
 
         # Specify filenames with syntaxt errors
-        if file_name in ["testfiles/basic_tests_123/proj2_man_syntaxErr_invalidPointerDeclaration2.c","testfiles/basic_tests_123/proj2_man_syntaxErr_variable.c","testfiles/basic_tests_123/proj2_man_syntaxErr_pointerOperations2.c"
-                         ,"testfiles/basic_tests_123/proj2_man_semanticErr_redeclaration.c","testfiles/advanced_tests/SemanticErrors/incompatibleTypes7.c","testfiles/basic_tests_123/proj2_man_semanticErr_undeclaredVariable3.c","testfiles/basic_tests_123/proj2_man_syntaxErr_variableConst2.c"]:
+        if file_name in ["testfiles/basic_tests_123/proj2_man_syntaxErr_invalidPointerDeclaration2.c",
+                         "testfiles/basic_tests_123/proj2_man_syntaxErr_variable.c",
+                         "testfiles/basic_tests_123/proj2_man_syntaxErr_pointerOperations2.c"
+            , "testfiles/basic_tests_123/proj2_man_semanticErr_redeclaration.c",
+                         "testfiles/advanced_tests/SemanticErrors/incompatibleTypes7.c",
+                         "testfiles/basic_tests_123/proj2_man_semanticErr_undeclaredVariable3.c",
+                         "testfiles/basic_tests_123/proj2_man_syntaxErr_variableConst2.c"]:
             return
 
         if file_name.startswith("testfiles/advanced_tests"):
@@ -45,13 +52,12 @@ class CatchAllTest(unittest.TestCase):
         out = subprocess.run(f"""clang-14 -S -emit-llvm {file_name} -o {file_name[:-2]}.ll""",
                              shell=True, capture_output=True)
 
-
         valid = True
 
         try:
-            main([0, "--input", file_name, "--render_ast", file_name[:-2]+"ASTVisual", "--render_symb", file_name[
-                                                                                                        :-2] + "SymbolTable",
-                                                                         "--target_llvm", file_name[:-2] + "LLVM.ll"], True)
+            main([0, "--input", file_name, "--render_ast", file_name[:-2] + "ASTVisual", "--render_symb", file_name[
+                                                                                                          :-2] + "SymbolTable",
+                  "--target_llvm", file_name[:-2] + "LLVM.ll"], True)
 
             if len(out.stderr) >= 1:
                 if "error" in out.stderr or "ERROR" in out.stderr:
@@ -65,8 +71,3 @@ class CatchAllTest(unittest.TestCase):
         if not valid:
             print(out.stderr)
             exit(1)
-
-
-
-
-
