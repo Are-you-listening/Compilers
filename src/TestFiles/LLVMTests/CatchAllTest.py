de47a119e88b115a3d9335a3ff9c7931b25a59c8
watson <kars@duck.com>
2024-03-20 15:06:56 +0100

    cleaned up LLVM tests

--- src/TestFiles/LLVMTests/CatchAllTest.py
+++ src/TestFiles/LLVMTests/CatchAllTest.py
@@ -1,17 +1,14 @@
-import filecmp
-import unittest
-from src.main.__main__ import main
 import os
-import sys
-import logging
+import unittest
 import subprocess
+from src.main.__main__ import main
 from src.llvm_target.LLVMSingleton import *
 
 
 class CatchAllTest(unittest.TestCase):
     def testEvaluateResults(self):
         """
-        This testcase will run all files in the testfiles/ folder
+        This testcase will run all files in the testfiles/ folder and checks if the amount of errors are the same
         :return:
         """
 
@@ -23,36 +20,20 @@ class CatchAllTest(unittest.TestCase):
                     print(filename)
                     self.runAST(filename)
 
-
-
     def runAST(self, file_name):
+        """
+        Run our actual compiler and catch its output
+        :param file_name:
+        :return:
+        """
+        LLVMSingleton.getInstance().clear()  # Make sure to reset the singleton instance
+        valid = True
 
-        LLVMSingleton.getInstance().clear()
-
-        # Specify filenames we don't want to run for now
-        if file_name in ["testfiles/basic_tests_123/proj2_man_pass_conversionExplicitTypeCast.c"]:
-            return
-
-        if file_name.startswith("testfiles/ownTests/proj1"):
-            return
-
-        # Specify filenames with syntaxt errors
-        if file_name in ["testfiles/basic_tests_123/proj2_man_syntaxErr_invalidPointerDeclaration2.c",
-                         "testfiles/basic_tests_123/proj2_man_syntaxErr_variable.c",
-                         "testfiles/basic_tests_123/proj2_man_syntaxErr_pointerOperations2.c"
-            , "testfiles/basic_tests_123/proj2_man_semanticErr_redeclaration.c",
-                         "testfiles/advanced_tests/SemanticErrors/incompatibleTypes7.c",
-                         "testfiles/basic_tests_123/proj2_man_semanticErr_undeclaredVariable3.c",
-                         "testfiles/basic_tests_123/proj2_man_syntaxErr_variableConst2.c"]:
-            return
-
-        if file_name.startswith("testfiles/advanced_tests"):
+        if file_name.startswith("testfiles/ownTests/proj1") or file_name.startswith("testfiles/advanced_tests"):  # Don't want to run those bad boys for now
             return
 
         out = subprocess.run(f"""clang-14 -S -emit-llvm {file_name} -o {file_name[:-2]}.ll""",
                              shell=True, capture_output=True)
-
-        valid = True
 
         try:
             main([0, "--input", file_name, "--render_ast", file_name[:-2] + "ASTVisual", "--render_symb", file_name[
@@ -64,7 +45,6 @@ class CatchAllTest(unittest.TestCase):
                     valid = False
 
         except:
-
             if len(out.stderr) == 0:
                 valid = False
 
---
+++ src/TestFiles/LLVMTests/CompareAll.py
@@ -0,0 +1,54 @@
+import os
+import filecmp
+import unittest
+from src.main.__main__ import main
+from src.llvm_target.LLVMSingleton import *
+
+
+class CompareAll(unittest.TestCase):
+    """
+    Test case to create a llvm output for each file and compare it with the corresponding "right" file. Right in the
+    sense that we pre-set an output we see as valid.
+    """
+    def testEvaluateResults(self):
+        path = "testfiles/"
+        for root, dirs, filenames in os.walk(path):
+            for file in filenames:
+                if file.endswith(".c"):
+                    filename = root + "/" + file
+                    print(filename)
+                    self.runAST(root + "/" + file)
+                    assert self.compareLLVM(filename[:-2] + "LLVMtoCompare.ll", filename)
+
+    @staticmethod
+    def createEmptyLLVMFile(filename):
+        """
+        :param filename:
+        :return:
+        """
+        filename = filename[:-2] + "LLVMtoCompare.ll"
+        with open(filename, 'w'):
+            pass
+
+    @staticmethod
+    def compareLLVM(newly, right):
+        """
+        Compare the contents of 2 files
+        :param newly:
+        :param right:
+        :return: True if they are the same
+        """
+        return filecmp.cmp(newly, right)
+
+    @staticmethod
+    def runAST(file_name: str):
+        """
+        Run our compiler to generate LLVM output
+        :param file_name:
+        :return:
+        """
+        LLVMSingleton.getInstance().clear()  # Make sure to reset the singleton service
+
+        # Create LLVM file
+        main([0, "--input", file_name, "--render_ast", file_name[:-2] + "ASTVisual", "--render_symb", file_name[:-2] + "SymbolTable",
+              "--target_llvm", file_name[:-2] + "LLVM.ll"], True)
