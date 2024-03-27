d85a6c3ebe40e24cc637533fb0d94171509e2d3d
watson <kars@duck.com>
2024-03-27 23:00:55 +0100

    tests

--- src/TestFiles/ASTTests/SyntaxErrorTests/testSyntaxError.py
+++ src/TestFiles/ASTTests/SyntaxErrorTests/testSyntaxError.py
@@ -30,7 +30,7 @@ from src.parser.ASTTableCreator import *
 
 class TestSyntaxError(unittest.TestCase):
     def testSyntaxErrors(self):
-        file_indexes = range(1, 27)
+        file_indexes = range(1, 29)
 
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
---
+++ testfiles/ownTests/p5.c
@@ -0,0 +1,8 @@
+int main() {
+//Incompatible Types?
+int x = 478;
+int b = -251454;
+
+int** x_ptr = &x;
+x_ptr = &b;
+}
