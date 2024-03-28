a957387c269666624d8725af8fd93bf486d4e4fc
watson <kars@duck.com>
2024-03-28 23:20:54 +0100

    Changes by tibov

--- script.py
+++ script.py
@@ -4,4 +4,10 @@ from src.llvm_target.LLVMSingleton import *
 for i in range(1, 13):
     file_name = f"example_source_files/test{i}"
     LLVMSingleton.getInstance().clear()  # Make sure to reset the singleton service
-    main([0, "--input", file_name+'.c', "--target_llvm", file_name+'.ll', "--render_symb", file_name+'table.dot', "--render_ast", file_name+'ast.dot'])
+
+
+    try:
+        main([0, "--input", file_name+'.c', "--target_llvm", file_name+'.ll', "--render_symb", file_name+'table.dot', "--render_ast", file_name+'ast.dot'])
+    except Exception as e:
+        print(e)
+        print(e.args)
