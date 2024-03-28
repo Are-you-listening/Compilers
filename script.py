fa5fc1cf63125f5b0130e1791944744312b2b7ee
watson <kars@duck.com>
2024-03-28 23:26:55 +0100

    finalising the examples

--- script.py
+++ script.py
@@ -1,13 +1,16 @@
 from src.main.__main__ import main
 from src.llvm_target.LLVMSingleton import *
 
-for i in range(1, 13):
-    file_name = f"example_source_files/test{i}"
-    LLVMSingleton.getInstance().clear()  # Make sure to reset the singleton service
 
+def main():
+    for i in range(1, 13):
+        file_name = f"example_source_files/test{i}"
+        LLVMSingleton.getInstance().clear()  # Make sure to reset the singleton service
 
-    try:
-        main([0, "--input", file_name+'.c', "--target_llvm", file_name+'.ll', "--render_symb", file_name+'table.dot', "--render_ast", file_name+'ast.dot'])
-    except Exception as e:
-        print(e)
-        print(e.args)
+        try:
+            main([0, "--input", file_name + '.c', "--target_llvm", file_name + '.ll', "--render_symb",
+                  file_name + 'table.dot', "--render_ast", file_name + 'ast.dot', "--target_control_flow", file_name+'cfg.dot'])
+        except:
+            pass
+
+main()
