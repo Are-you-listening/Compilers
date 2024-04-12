cf8d45a94633e848896fd8090adb7d2715493551
emiilll <emil.lambert@student.uantwerpen.be>
2024-04-12 09:50:43 +0200

    BlacklistVisitor fix

--- src/parser/BlacklistVisitor.py
+++ src/parser/BlacklistVisitor.py
@@ -4,16 +4,16 @@ from src.parser.ASTVisitor import *
 class BlacklistVisitor(ASTVisitor):
     def __init__(self):
         self.blacklist = ['(', ')', ';', '{', '}', ',']
+        self.toRemove = set()
 
     def visitNode(self, node: ASTNode):
         pass
 
     def visitNodeTerminal(self, node: ASTNodeTerminal):
-        index = 0
         if node.text in self.blacklist:
-            for child in node.parent.children:
-                if child == node:
-                    node.parent.children.pop(index)
-                    node.parent = None
-                    break
-                index += 1
+            self.toRemove.add(node)
+
+    def visit(self, ast: AST):
+        self.preorder(ast.root)
+        for node in self.toRemove:
+            node.parent.removeChild(node)
