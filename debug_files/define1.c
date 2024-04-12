9ab450598b2fe638b02480b8784ab66a0465a4f5
watson <kars@duck.com>
2024-04-12 17:04:31 +0200

    worked on #define

---
+++ debug_files/define1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Defining parameterized macros with expression
+#define CIRCLE_AREA 3.14
+#define bool const int
+#define True 1
+#define False 0
+#define appel int**
+
+int** x;
+
+int main()
+{
+
+    int radius = 21;
+    int side = 5;
+    int area;
+    bool success = True;
+
+    // Using macros to calculate areas by
+    // passing argument
+    area = CIRCLE_AREA;
+    printf("Area of Circle of radius %d: %d \n", radius,
+           area);
+
+
+    return 0;
+}
