d709779e1979e93e4eedabd2a4362038b02c055c
watson <kars@duck.com>
2024-04-22 23:41:29 +0200

    add explanation

--- testfilesTODO/cry1.c
+++ testfilesTODO/cry1.c
@@ -5,7 +5,7 @@ int True = 1;
 
 int main()
 {
-    int True = True - 20;
+    int True = True - 20; // True is here not undeclared; it can come from a higher scope but in our checks it will find the LHS True
     int success = True;
 
     printf("%d", success);
