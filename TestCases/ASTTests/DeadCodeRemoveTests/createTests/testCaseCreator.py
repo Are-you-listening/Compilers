d3020d81777924b129d89d0e16907431f2e87f85
watson <kars@duck.com>
2024-04-23 13:35:36 +0200

    brought back testcase creators for asttests

---
+++ TestCases/ASTTests/DeadCodeRemoveTests/createTests/testCaseCreator.py
@@ -0,0 +1,108 @@
+from antlr4 import *
+from src.antlr_files.grammarCLexer import grammarCLexer
+from src.antlr_files.grammarCParser import grammarCParser
+from src.parser.ASTCreator import ASTCreator
+from src.parser.DotVisitor import *
+from src.parser.Constraints.ConstraintChecker import *
+from src.parser.ValueAdderVisitor import *
+from src.parser.ASTDereferencer import *
+from src.parser.ASTConversion import *
+from src.parser.ASTCleaner import *
+from src.parser.ASTCleanerAfter import *
+from src.parser.Tables.TableDotVisitor import *
+from src.parser.CodeGetter import *
+from TestCases.ABCTests.AstLoader import AstLoader
+from src.parser.ASTTableCreator import ASTTableCreator
+from src.parser.ASTLoopCleaner import *
+from src.parser.ASTIfCleaner import *
+from src.parser.ConstantStatementFolding import *
+from src.parser.DeadCodeRemover import *
+from src.llvm_target.ControlFlowCreator import *
+from src.parser.VirtualLineNrVisitor import *
+from src.parser.BlacklistVisitor import *
+
+
+input_file = "read_file.c"
+
+input_stream = FileStream(input_file)  # Declare some variables
+lexer = grammarCLexer(input_stream)
+stream = CommonTokenStream(lexer)
+parser = grammarCParser(stream)
+
+lexer.removeErrorListeners()
+lexer.addErrorListener(EListener())
+
+parser.removeErrorListeners()  # Add our own error Listener
+parser.addErrorListener(EListener())
+tree = parser.start_()
+
+toAST = ASTCreator(lexer)  # Create Actual AST
+toAST.visit(tree)
+ast = toAST.getAST()
+
+virtualline = VirtualLineVisitor()
+virtualline.visit(ast)
+
+black_list_visitor = BlacklistVisitor()
+black_list_visitor.visit(ast)
+
+codegetter = CodeGetter()  # Link each line of code to a line number
+codegetter.visit(ast)
+
+ASTTypedefReplacer().visit(ast)  # Replace all uses of typedefs
+
+ASTIfCleaner().visit(ast)  # Do a cleanup of the if statements
+ASTLoopCleaner().visit(ast)  # Cleanup For/While loops
+
+ASTCleaner().visit(ast)  # Do a standard cleaning
+
+ASTTableCreator().visit(ast)  # Create the symbol table
+
+ASTCleanerAfter().visit(ast)  # Clean even more :)
+
+ASTDereferencer().visit(ast)  # Correct the use of references & pointers into our format
+
+ConstraintChecker(True).visit(ast)  # Checkup Semantic & Syntax Errors
+
+ConstantFoldingVisitor().visit(ast)
+
+ASTConversion().visit(ast)
+
+ValueAdderVisitor().visit(ast)
+
+ConstantStatementFolding().visit(ast)
+
+
+"""
+add needed stuff above
+"""
+
+json = AstLoader.store(ast)
+with open("file_read_json.json", "wt") as f:
+    f.write(json)
+
+
+"""
+add check stuff
+"""
+
+cfc = ControlFlowCreator()
+cfc.visit(ast)
+
+d = DotVisitor("read_file_before")  # Export AST in Dot
+d.visit(ast)
+
+
+DeadCodeRemover().visit(ast)  # removes dead code inside a block coming after a return/continue or break
+
+"""
+add check stuff above
+"""
+
+json = AstLoader.store(ast)
+with open("file_read_json_result.json", "wt") as f:
+    f.write(json)
+ast_tree = AstLoader.load(json)
+
+d = DotVisitor("read_file_after")  # Export AST in Dot
+d.visit(ast_tree)
--- debug_files/define4.c
+++ debug_files/define4.c
@@ -1,5 +1,22 @@
-printf("%d", 1);    // also stopped by the grammar
+#include <stdio.h>
 
-int main(){
+#define CIRCLE_AREA 3.14
+#define bool const int
+#define True 1
+#define False 0
+#define appel int**
+
+int main()
+{
+
+    int radius = 21;
+    int side = 5;
+    int area;
+    bool success = True;
+
+    area = CIRCLE_AREA;
+    printf("%d%d%d", radius,
+           area, success);
+
     return 0;
 }
