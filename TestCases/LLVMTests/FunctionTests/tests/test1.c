5716850ae27356bdb1e968a701e72be4bbc4e938
Lucas Vanden Abeele <lucas.vanden.abeele@hotmail.com>
2024-04-25 21:22:00 +0200

    Added some error test for functions

---
+++ TestCases/LLVMTests/FunctionTests/tests/test1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int func(int a, int b);
+
+int main() {
+    int y = 5;
+    int u = 10;
+    int c;
+    c = func(y, u);
+    printf("%d\n", c);
+    return y;
+}
---
+++ TestCases/LLVMTests/FunctionTests/tests/test2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int y = 5;
+    int u = 10;
+    int c;
+    c = func(y, u);
+    printf("%d\n", c);
+    return y;
+}
