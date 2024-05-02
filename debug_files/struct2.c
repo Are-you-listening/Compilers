31ce28dcfe85ad90b148e6c66b23b7da6aaebe8d
watson <kars@duck.com>
2024-05-02 21:45:38 +0200

    yay

--- debug_files/struct2.c
+++ debug_files/struct2.c
@@ -1,62 +1,22 @@
 #include <stdio.h>
 
+struct kaas{
+    int melk; //Melk
+    float yoghurt;
+};
+
+struct kaas* structCreator(int melk, float yoghurt){ //Struct Creator
+    struct kaas gouda;
+    gouda.melk = melk;
+    gouda.yoghurt = yoghurt;
+    struct kaas* ptr = &gouda;
+    return ptr;
+}
+
 int main() {
-    int a = 42 + 17;
-    int c = 100 - 37 + a;
-    int d = 5 * 8;
-    int e = 64 / 4;
-    int f = 25 % 7;
-    int g = 2 & 5;
-    int h = 7 | 3;
-    int i = 9 ^ 14;
-    int j = ~42;
-    int k = 8 << 2;
-    int l = 16 >> 3;
-    int m = (5 > 3) && (10 < 20);
-    int n = (7 >= 6) || (3 != 3);
-    int o = !(8 <= 5);
-    int p = (4 < 7) - 42 / 24;
-    int q = 8 << 2;
-    int r = 16 >> 3;
-    int s = 12 & 7;
-    int t =5 | 3;
-    int u = ~478;
-    int v = 8 << 3;
-    int w = 16 >> 3;
-    int x = (5 > 3) && (10 < 20);
-    int y = (7 >= 6) || (3 != 3);
-    int z = !(8 <= 5);
-    int sofa = 9876543210123456789 + 1234567890987654321;
-    int appel = ~1234567890123456789;
+    struct kaas* ptr; //kaas* ptr definition in main
 
-    printf("%d", a);
-    printf("%d", c);
-    printf("%d", d);
-    printf("%d", e);
-    printf("%d", f);
-    printf("%d", g);
-    printf("%d", h);
-    printf("%d", i);
-    printf("%d", j);
-    printf("%d", k);
-    printf("%d", l);
-    printf("%d", m);
-    printf("%d", n);
-    printf("%d", o);
-    printf("%d", p);
-    printf("%d", q);
-    printf("%d", r);
-    printf("%d", s);
-    printf("%d", t);
-        printf("%d", u);
-            printf("%d", v);
-                printf("%d", w);
-                    printf("%d", x);
-                        printf("%d", y);
-                            printf("%d", z);
-                                printf("%d", sofa);
-                                    printf("%d", appel);
-
-
+    ptr = structCreator(10, 20.0);
+    printf("%d %f ", ptr->melk, ptr->yoghurt);
     return 0;
 }
