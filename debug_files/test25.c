0fd2120663220d094f0bfef58266f878fae1ed68
tibov <tibo.verreycken@telenet.be>
2024-05-03 11:47:37 +0200

    fixed constant folding tests

--- debug_files/test25.c
+++ debug_files/test25.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-int a[2][3];
-
 
 int main(){
-	int x = 3;
-	a[0][0] = 1;
-	a[1][0] = 2;
-	printf("%d; %d; %d;", a[0][0], a[1][0], x);
-	return 0;
+    int* k;
+    int result = printf("%d", 1);
+    float lol = printf("%d", result);
+    printf("%f", lol);
+    return 0;
 }
