03fc3844dfe4396ed744021539ba4844b6dd13ef
lucas <lucas.vanden.abeele@hotmail.com>
2024-05-03 16:56:35 +0200

    Added VERY fun combined tests, to print patters, sort arrays,...

---
+++ TestCases/LLVMTests/CombinedTests/tests/test4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void printPyramid(int rows, char toPrint)
+{
+
+   int i = 1;
+   while (i <= rows) {
+        int j = 1;
+      while (j < i) {
+         printf("%c", toPrint);
+         j++;
+      }
+        printf("%c\n", toPrint);
+      i++;
+   }
+}
+
+int main() {
+    char toPrint = '*';
+    int rows = 5;
+    printPyramid(rows, toPrint);
+   return 0;
+}
