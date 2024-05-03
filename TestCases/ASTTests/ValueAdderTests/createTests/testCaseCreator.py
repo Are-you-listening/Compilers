ddc09a30bb0373601c17d7897699beefbf54a4c1
emiilll <emil.lambert@student.uantwerpen.be>
2024-05-03 17:02:03 +0200

    new main in valueAdder testCaseCreator.py

--- TestCases/ASTTests/ValueAdderTests/createTests/testCaseCreator.py
+++ TestCases/ASTTests/ValueAdderTests/createTests/testCaseCreator.py
@@ -2,8 +2,19 @@ from antlr4 import *
 from src.antlr_files.grammarCLexer import grammarCLexer
 from src.antlr_files.grammarCParser import grammarCParser
 from src.parser.ASTCreator import ASTCreator
+from src.parser.ASTIfCleaner import ASTIfCleaner
+from src.parser.ASTLoopCleaner import ASTLoopCleaner
+from src.parser.ArrayCleaner import ArrayCleaner
+from src.parser.Constraints.CheckRvalues import CheckRvalues
 from src.parser.DotVisitor import *
 from src.parser.Constraints.ConstraintChecker import *
+from src.parser.EnumConverter import EnumConverter
+from src.parser.FunctionPtrCleaner import FunctionPtrCleaner
+from src.parser.PointerReformater import PointerReformater
+from src.parser.StringToArray import StringToArray
+from src.parser.SwitchConverter import SwitchConverter
+from src.parser.TypeMerger import TypeMerger
+from src.parser.UnarySaVisitor import UnarySaVisitor
 from src.parser.ValueAdderVisitor import *
 from src.parser.ASTDereferencer import *
 from src.parser.ASTConversion import *
@@ -17,6 +28,8 @@ from src.parser.VirtualLineNrVisitor import VirtualLineVisitor
 from src.parser.BlacklistVisitor import BlacklistVisitor
 from src.parser.StructCleaner import StructCleaner
 from src.parser.StructCleanerAfter import StructCleanerAfter
+from src.parser.DynamicAllocation import DynamicAllocation
+from src.parser.FileIO import FileIO
 
 input_file = "read_file.c"
 
@@ -37,40 +50,65 @@ ast = toAST.getAST()
 below add needed stuff
 """
 
-virtualline = VirtualLineVisitor()
-virtualline.visit(ast)
+virtualLine = VirtualLineVisitor()
+virtualLine.visit(ast)
 
-black_list_visitor = BlacklistVisitor()
-black_list_visitor.visit(ast)
-
-codegetter = CodeGetter()
+codegetter = CodeGetter()  # Link each line of code to a line number
 codegetter.visit(ast)
+
+BlacklistVisitor().visit(ast)
+
+PointerReformater().visit(ast)
+
+ASTLoopCleaner().visit(ast)  # Cleanup For/While loops
 
 StructCleaner().visit(ast)  # Massage the structs
 
+EnumConverter().visit(ast)  # Convert enum to typedef & const bools
+TypeMerger().visit(ast)  # Reformat enum & struct declarations to our format
+
+#DotVisitor("output/debug0").visit(ast)  # Export AST in Dot
+
 ASTTypedefReplacer().visit(ast)  # Replace all uses of typedefs
 
-astcleaner = ASTCleaner()  # Do a standard cleaning
-astcleaner.visit(ast)
+ASTIfCleaner().visit(ast)  # Do a cleanup of the if statements
 
+ASTCleaner().visit(ast)  # Do a standard cleaning
+
+SwitchConverter().visit(ast)  # convert switch statement to if else
+
+StringToArray().visit(ast)
+
+FunctionPtrCleaner().visit(ast) #  cleans the function ptrs
+
+ArrayCleaner().visit(ast)
+
+#DotVisitor("output/debug0").visit(ast)  # Export AST in Dot
 ASTTableCreator().visit(ast)  # Create the symbol table
+
+DynamicAllocation.add_allocation(ast)
+FileIO.add_io(ast)
 
 StructCleanerAfter().visit(ast)
 
-astcleanerafter = ASTCleanerAfter()  # Do a standard cleaning
-astcleanerafter.visit(ast)
+ASTCleanerAfter().visit(ast)  # Clean even more :)
 
-ast_deref = ASTDereferencer()  # Correct the use of references & pointers into our format
-ast_deref.visit(ast)
+CheckRvalues().visit(ast)
+
+#DotVisitor("output/debug0").visit(ast)  # Export AST in Dot
+ASTDereferencer().visit(ast)
 
 constraint_checker = ConstraintChecker(True)  # Checkup Semantic & Syntax Errors
 constraint_checker.visit(ast)
 
-cfv = ConstantFoldingVisitor()
-cfv.visit(ast)
 
 ast_conv = ASTConversion()
 ast_conv.visit(ast)
+
+UnarySaVisitor().visit(ast)
+
+cfv = ConstantFoldingVisitor()
+cfv.visit(ast)
 
 """
 add needed stuff above
