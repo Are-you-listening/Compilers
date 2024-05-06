5ced6e138e35c1e1204963a537df5ac5aa50606e
tibov <tibo.verreycken@telenet.be>
2024-05-06 19:06:41 +0200

    fix

--- TestCases/LLVMTests/StringTests/tests/test12.c
+++ TestCases/LLVMTests/StringTests/tests/test12.c
@@ -5,7 +5,7 @@
 int main(){
     char z[2];
     z[0] = 'O';
-    z[1] = "Y"; // Invalid llvm bitcase because the RHS is translated as an array (notice the doubel quotes "!), we can't bitcased array types to i8*
+    z[1] = "Y";
     printf("%c", z[1]);
     return 0;
 }
