f5ad19c6364183faccfe5e640ef15adfe4fe8f45
lucas <lucas.vanden.abeele@hotmail.com>
2024-05-06 19:34:41 +0200

    nog een aantal example files

--- debug_files/test15.c
+++ debug_files/test15.c
@@ -1,16 +1,9 @@
-#include <stdio.h>
-
-#define NULL 0
-
-void func1() {
-    printf("d%", 1);
-}
-
-void func2() {
-    printf("d%", 2);
-}
-
-int main() {
-    void (*funcPtr)() = NULL;
+int main(){
+    float a = 5.0;
+    int b = 5;
+    char c = 'e';
+    float* d = &5.0;
+    int* e = &4;
+    char* f = &'a';
     return 0;
 }
