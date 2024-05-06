b05b0884ba3b198c2f9f872099b4992d76680a2c
watson <kars@duck.com>
2024-05-06 12:48:59 +0200

    updated io constrained to support other io methods

--- src/parser/Constraints/IOConstraint.py
+++ src/parser/Constraints/IOConstraint.py
@@ -13,9 +13,11 @@ class IOConstraint(Constraint):
         self.includeSTDIO = includeSTDIO
 
     def checkNode(self, node: ASTNode):
-        if node.text in ["printf", "scanf"]:
+        if node.text in ["printf", "scanf", "fgets", "fopen", "fclose", "fputs"]:
             if not self.includeSTDIO:  # Check if stdio is included
                 ErrorExporter.undeclaredVariable(node.text, node.position, "function")
+            else:
+                self.accepted = True
 
     def checkTerminalNode(self, node: ASTNodeTerminal):
         pass
