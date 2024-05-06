ca83366e401f2117024c7d9d06904740119a4d0b
lucas <lucas.vanden.abeele@hotmail.com>
2024-05-06 17:09:06 +0200

    nog een aantal example files

---
+++ example_source_files/11_pointers.c
@@ -0,0 +1,9 @@
+int main(){
+    float a = 5.0;
+    int b = 5;
+    char c = 'e';
+    float* d = &a;
+    int* e = &b;
+    char* f = &c;
+    return 0;
+}
---
+++ example_source_files/14_implicit_conversions.c
@@ -0,0 +1,6 @@
+int main() {
+    int a = 10.2;
+    char c = 5;
+    float b = 10;
+    return 0;
+}
