5d701c7e7ea11bef729d8240c08a8ddf03124f70
watson <kars@duck.com>
2024-05-06 18:42:56 +0200

    made testcases lower case

--- script.py
+++ script.py
@@ -1,18 +1,24 @@
 from src.main.__main__ import main
 from src.llvm_target.LLVMSingleton import *
+import os
 
 
 def script():
-    for i in range(1, 14):
-        file_name = f"example_source_files/test{i}"
-        LLVMSingleton.getInstance().clear()  # Make sure to reset the singleton service
+    directory = os.path.dirname(os.path.abspath(__file__))  # Get directory file is in
+    os.chdir(directory)  # Change the dir to only focus on this test
+    directory += "/example_source_files"  # Walk through the testfiles
 
-        try:
-            main([0, "--input", file_name + '.c', "--target_llvm", file_name + '.ll', "--render_symb",
-                  file_name + 'table.dot', "--render_ast", file_name + 'ast.dot', "--target_control_flow",
-                  file_name + 'cfg.dot'])
-        except:
-            pass
+    print(directory)
 
+    for file in os.listdir(directory):  # Loop through all files
+        if os.path.splitext(file)[-1] != '.c':  # We only run c files
+            continue
+
+        print(file)
+
+            #LLVMSingleton.getInstance().clear()  # Make sure to reset the singleton service
+
+            # Run our llvm
+            #main([0, "--input", file_name, "--target_llvm", f"{file_name[:-2]}.ll", "--fold", "True"])  # Run our compiler with folding
 
 script()
