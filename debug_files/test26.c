f2167972d1bfb9dc1dad985ccdadce329e076364
tibov <tibo.verreycken@telenet.be>
2024-05-18 16:23:07 +0200

    does this change things

--- debug_files/test26.c
+++ debug_files/test26.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
-int v(int b[2][2]){
-    printf("%c", b[0][0]);
-    printf("%c", b[0][1]);
-    printf("%c", b[1][0]);
-    printf("%c", b[1][1]);
-    return 0;
-}
+
 
 int main(){
-    char a[2][2] = {{'a', 'b'}, {'c', 'd'}};
-
-    v(a);
-
+    int a[2][2] = {{'a', 'b'}, {'c', 'd'}};
+    printf("%c", a[0][0]);
+    printf("%c", a[0][1]);
+    printf("%c", a[1][0]);
+    printf("%c", a[1][1]);
     return 0;
 }
