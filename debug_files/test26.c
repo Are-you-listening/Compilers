ce98dde72435c4505c55870eea62401166af82af
tibov <tibo.verreycken@telenet.be>
2024-05-20 19:39:57 +0200

    fixed a bug

--- debug_files/test26.c
+++ debug_files/test26.c
@@ -1,38 +1,10 @@
 #include <stdio.h>
 
-enum Direction {
-NORTH,
-EAST,
-SOUTH,
-WEST,
-};
+int main() {
 
-int main()
-{
-/* NORTH++;  This should thrown an semantic error because */
-/* const int* appel = &peer; This should also give an error */
-int peer = 1;
-int* appel = &peer;
-appel--;
---appel;
+    int x[2][3];
 
-appel++;
-++appel;
-printf("%d ", peer);
-
-(*appel)--;
-printf("%d ", peer);
-(*appel)++;
-printf("%d ", peer);
-
---(*appel);
-printf("%d ", peer);
-
---appel;
-
-/* --(peer+peer); This is not allowed /
---(peer);
---(appel);
-/ appel + --appel; This returns an semantic error*/
-return 0;
+    x[1][2] = 1;
+    printf("%d", x[1][2]);
+    return 0;
 }
