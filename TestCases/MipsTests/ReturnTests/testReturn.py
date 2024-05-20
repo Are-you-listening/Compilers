b9da1cb4697824d5b6320607dfb6621409d236c0
tibov <tibo.verreycken@telenet.be>
2024-05-20 10:47:49 +0200

    return tests

--- TestCases/MipsTests/ReturnTests/testReturn.py
+++ TestCases/MipsTests/ReturnTests/testReturn.py
@@ -1,10 +1,10 @@
 from TestCases.ABCTests.abcTest import *
 
 
-# class ReturnTests(MipsTest):
-#     """
-#     Test case to run all created llvm output
-#     """
-#
-#     def test(self):
-#         MipsTest.LLVM_test(self, os.path.abspath(__file__), False)  # Call the base class tests function
+class ReturnTests(MipsTest):
+     """
+     Test case to run all created llvm output
+     """
+
+     def test(self):
+         MipsTest.LLVM_test(self, os.path.abspath(__file__), False)  # Call the base class tests function
