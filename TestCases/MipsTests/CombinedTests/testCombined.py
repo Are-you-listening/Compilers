cf03956ee9f0fdc68253c58a911fd78dc8cdfd63
tibov <tibo.verreycken@telenet.be>
2024-05-20 10:45:20 +0200

    combined tests

--- TestCases/MipsTests/CombinedTests/testCombined.py
+++ TestCases/MipsTests/CombinedTests/testCombined.py
@@ -1,14 +1,14 @@
 from TestCases.ABCTests.abcTest import *
 
 
-# class CombinedTests(MipsTest):
-#     """
-#     Test case to run all created llvm output
-#     """
-#     def compileGCC(self, file_name):
-#         c_out = subprocess.run(f"""gcc {file_name} -o temp/temp""",
-#                                    shell=True, capture_output=True)
-#         return c_out
-#
-#     def test(self):
-#         MipsTest.LLVM_test(self, os.path.abspath(__file__), False)   # Call the base class tests function
+class CombinedTests(MipsTest):
+     """
+     Test case to run all created llvm output
+     """
+     def compileGCC(self, file_name):
+         c_out = subprocess.run(f"""gcc {file_name} -o temp/temp""",
+                                    shell=True, capture_output=True)
+         return c_out
+
+     def test(self):
+         MipsTest.LLVM_test(self, os.path.abspath(__file__), False)   # Call the base class tests function
