bc2aaa50a9ea64b34c1a27030a0a65eb7f22828f
lucas <lucas.vanden.abeele@hotmail.com>
2024-05-23 23:14:55 +0200

    this test is fine now

--- debug_files/test15.c
+++ debug_files/test15.c
@@ -1,36 +1,23 @@
 #include <stdio.h>
 
+#define NULL 0.0
 
-int main() {
-const int b = -6250;
-const int x = 5;
+void printPositive(float a)
+{
+    if (a > NULL)
+    {
+        printf("%d", (int) a);
+    }
+}
 
-
-
-int* non_const_pointer = &x;
-
-printf("%d", *non_const_pointer);
-
-*non_const_pointer = 36941;
-
-printf("%d", *non_const_pointer);
-
-non_const_pointer = &b;
-
-printf("%d", *non_const_pointer);
-
-
-char c = 'x';
-char nl = '\n';
-
-char* char_ptr = &c;
-printf("%c", *char_ptr);
-
-*char_ptr = '\t';
-printf("%c", *char_ptr);
-char_ptr = &nl;
-printf("%c", *char_ptr);
-
+int main ()
+{
+    float arr[5] = {-10.20, -20.125, 31.26, 0.00, 15.2};
+    int i = 0;
+    while ( i < 5)
+    {
+        printPositive(arr[i]);
+        i++;
+    }
     return 0;
-
 }
