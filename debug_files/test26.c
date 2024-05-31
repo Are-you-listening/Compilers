78e290df83c776a0c92fab18b3ceebf9f0fe6d0f
tibov <tibo.verreycken@telenet.be>
2024-05-31 11:18:29 +0200

    temp commit

--- debug_files/test26.c
+++ debug_files/test26.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char a[2][2][2];
+
 
 int main() {
+    char a[2][2][2];
+
     a[0][0][0] = 'a';
     a[1][0][0] = 'c';
     a[1][1][1] = 'b';
@@ -11,7 +13,7 @@ int main() {
     int* v = a;
 
     printf("%c", *(v));
-    //printf("%c", *(v+1));
+    printf("%c", *(v+1));
 
 
 
